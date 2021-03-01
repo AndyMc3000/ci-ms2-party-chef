a68ce6cee1f9932306d81d080dfa91f1902a77a3
AndyMc3000 <andypatmcd@gmail.com>
2021-03-01 22:12:58 +0000

    Add custom test marker and info window to Google map.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,35 +1,108 @@
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        mapId: "cae23a2b5aab6c26",
-        zoom: 6.5,
-        center: {
-            lat: 53.430659171975954,
-            lng: -7.946188576924455
-        }
-    });
+var map;
+var InfoOgj = [];
+var centerCoords = {
+    lat: 53.430659171975954,
+    lng: -7.946188576924455
+};
 
-    var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+var icons = {
+    kitchen: {
+        icon: 'https://i.ibb.co/xq9Y2X4/chef-2-icon-iconarchive.png',
+    },
+};
 
-    var locations = [{
-        lat: 51.91754579951517,
-        lng: -8.472666050361664
-    }, {
-        lat: 51.891463947246024,
-        lng: -8.48991998978755
-    }, {
-        lat: 40.754932,
-        lng: -73.984016
-    }];
+var markersOnMap = [
+    {
+        placeName: 'Cork (North) Kitchen',
+        address1: 'Unit 3',
+        address2: 'Blackpool Retail Park',
+        address3: 'Blackpool',
+        address4: 'Co.Cork',
+        postcode: 'P19 DZ32',
+        tel: '021-435 7892',
+        email: 'corknorth@partychef.ie',
+        image: 'https://i.ibb.co/xq9Y2X4/chef-2-icon-iconarchive.png',
+        LatLng: [{
+            lat: 51.91754579951517,
+            lng: -8.472666050361664
+        }],
+        type: 'kitchen'
+    },
+    {
+        placeName: 'Cork (South) Kitchen',
+        address1: 'Unit 3',
+        address2: 'Blackpool Retail Park',
+        address3: 'Blackpool',
+        address4: 'Co.Cork',
+        postcode: 'P19 DZ32',
+        tel: '021-435 7892',
+        email: 'corknorth@partychef.ie',
+        image: 'https://i.ibb.co/xq9Y2X4/chef-2-icon-iconarchive.png',
+        LatLng: [{
+            lat: 51.891463947246024,
+            lng: -8.48991998978755
+        }],
+        type: 'kitchen'
+    },
+];
 
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
+window.onload = function () {
+    initMap();
+};
+
+function addMarkerInfo() {
+    for( var i = 0; i < markersOnMap.length; i++ ) {
+        var contentString = '<h3>' + 
+                            markersOnMap[i].placeName + 
+                            '</h3>' + 
+                            '<div class="infoimage">' +
+                            '<img src=' +
+                            markersOnMap[i].image +
+                            '>' + 
+                            '</img>' +
+                            '</div>' +
+                            '<ul>' + 
+                            '<li>' + 
+                            markersOnMap[i].address1 + 
+                            '</li>' + 
+                            '<li>' + 
+                            markersOnMap[i].address2 + 
+                            '</li>' + '<li>' + 
+                            markersOnMap[i].address3 + 
+                            '</li>' + 
+                            '<li>' + 
+                            markersOnMap[i].address4 + 
+                            '</li>' + 
+                            '<li>' + 
+                            markersOnMap[i].postcode + 
+                            '</li>' + 
+                            '<li>' + 
+                            markersOnMap[i].tel + 
+                            '</li>' + 
+                            '<li>' + 
+                            markersOnMap[i].email + 
+                            '</li>' + 
+                            '</ul>';
+        const marker = new google.maps.Marker({
+            position: markersOnMap[i].LatLng[0],
+            icon: icons[markersOnMap[i].type].icon,
+            map: map
         });
-    });
 
-    new MarkerClusterer(map, markers, {
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-  });
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    }
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6.5,
+        center: centerCoords
+    });
+    addMarkerInfo();
 }
