39fad5f080c790f100c263698014de4aef2deaea
AndyMc3000 <andypatmcd@gmail.com>
2021-03-02 07:59:32 +0000

    Style Info Window content in Google Map.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -23,20 +23,20 @@ var markersOnMap = [
         email: 'corknorth@partychef.ie',
         image: 'https://i.ibb.co/xq9Y2X4/chef-2-icon-iconarchive.png',
         LatLng: [{
-            lat: 51.91754579951517,
-            lng: -8.472666050361664
+            lat: 51.90473191472518,
+            lng: -8.455334277325122
         }],
         type: 'kitchen'
     },
     {
         placeName: 'Cork (South) Kitchen',
-        address1: 'Unit 3',
-        address2: 'Blackpool Retail Park',
-        address3: 'Blackpool',
-        address4: 'Co.Cork',
+        address1: 'Hayfield Manor Hotel',
+        address2: 'Perrott Ave',
+        address3: 'College Rd',
+        address4: 'Cork',
         postcode: 'P19 DZ32',
-        tel: '021-435 7892',
-        email: 'corknorth@partychef.ie',
+        tel: '021-436 7892',
+        email: 'corksouth@partychef.ie',
         image: 'https://i.ibb.co/xq9Y2X4/chef-2-icon-iconarchive.png',
         LatLng: [{
             lat: 51.891463947246024,
@@ -52,9 +52,11 @@ window.onload = function () {
 
 function addMarkerInfo() {
     for( var i = 0; i < markersOnMap.length; i++ ) {
-        var contentString = '<h3>' + 
+        var contentString = '<div class="infoHeading">' +
+                            '<h3>' + 
                             markersOnMap[i].placeName + 
                             '</h3>' + 
+                            '</div>' +
                             '<div class="infoimage">' +
                             '<img src=' +
                             markersOnMap[i].image +
@@ -62,9 +64,9 @@ function addMarkerInfo() {
                             '</img>' +
                             '</div>' +
                             '<ul class="infoWindowList">' + 
-                            '<li>' + 
+                            '<li><h4>' + 
                             markersOnMap[i].address1 + 
-                            '</li>' + 
+                            '</h4></li>' + 
                             '<li>' + 
                             markersOnMap[i].address2 + 
                             '</li>' + '<li>' + 
@@ -76,12 +78,14 @@ function addMarkerInfo() {
                             '<li>' + 
                             markersOnMap[i].postcode + 
                             '</li>' + 
-                            '<li>' + 
+                            '<li><strong>' + 
                             markersOnMap[i].tel + 
-                            '</li>' + 
-                            '<li>' + 
+                            '</strong></li>' + 
+                            '<li>Email: <a href=mailto:"' +  
                             markersOnMap[i].email + 
-                            '</li>' + 
+                            '">' + 
+                            markersOnMap[i].email +
+                            '</a></li>' + 
                             '</ul>';
         const marker = new google.maps.Marker({
             position: markersOnMap[i].LatLng[0],
