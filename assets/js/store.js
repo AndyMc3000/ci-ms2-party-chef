e502d1da815cf3ac0291c7b8a011ddd71512cfc4
AndyMc3000 <andypatmcd@gmail.com>
2021-03-05 07:57:27 +0000

    Add content to MyMenu form.

--- assets/js/store.js
+++ assets/js/store.js
@@ -54,8 +54,7 @@ function addToCartClicked(event) {
     var shopItem = button.parentElement.parentElement
     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-    addItemToCart(title, price, imageSrc)
+    addItemToCart(title, price)
     updateCartTotal()
 }
 
@@ -72,13 +71,12 @@ function addItemToCart(title, price, imageSrc) {
     }
     var cartRowContents = `
         <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
             <span class="cart-item-title">${title}</span>
         </div>
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
+            <button class="btn btn-danger" type="button"><strong>X</strong></button>
         </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
