7f0284592dff7303fd47dd8140118f29be098d21
AndyMc3000 <andypatmcd@gmail.com>
2021-03-17 17:22:12 +0000

    Change background and text colors on index.html and modals.

--- assets/js/emailjs.js
+++ assets/js/emailjs.js
@@ -1,33 +1,42 @@
-const btn = document.getElementById('email-button');
+$(document).ready(function () {
+    const btn = document.getElementById('contact-us-submit-btn');
+    console.log(btn);
 
-console.log(btn);
+    document.getElementById('contact-us-form')
+        .addEventListener('submit', function (event) {
+            event.preventDefault();
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+            btn.value = 'Sending...';
+        
+            if (event.target.checkValidity() === false) {
+                event.target.classList.add('was-validated');
+                return;
+            }
+            
 
-   btn.value = 'Sending...';
+            let templateParams = {
+                first_name: document.getElementById('from_name').value,
+                surname: '',
+                tel_number: '',
+                email_address: '',
+                party_date: '',
+                party_location: '',
+                test: '',
+                options: '',
+            };
 
-   var templateParams = {
-    first_name: '',
-    surname: '', 
-    tel_number: '',
-    email_address: '',
-    party_date: '',
-    party_location: '',
-    test: '',
-    options: '',
-};
-console.log(btn);
+            console.log(btn);
 
-emailjs.send('default_service', 'template_cewgm4l', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-        purchaseClicked()
-        updateCartTotal()
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
-});
+            emailjs.send('default_service', 'template_cewgm4l', templateParams)
+                .then(function (response) {
+                    console.log('SUCCESS!', response.status, response.text);
+                    alert('Message Sent Successfully')
+//                    purchaseClicked()
+//                    updateCartTotal()
+                }, function (error) {
+                    console.log('FAILED...', error);
+                });
+        });
 
-console.log(btn);
+    console.log(btn);
+})
