166a11e1a5d6b3a6ec3ee606b01c13a43c338019
AndyMc3000 <andypatmcd@gmail.com>
2021-03-19 07:47:39 +0000

    Fix bugs where contact forms were not sending emails.

---
+++ assets/js/contact-page-contact-form.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    const btn = document.getElementById('contact-us-submit-btn');
+    console.log(btn);
+
+    document.getElementById('contact-us-form')
+        .addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            btn.value = 'Sending...';
+        
+            if (event.target.checkValidity() === false) {
+                event.target.classList.add('was-validated');
+                return;
+            }
+            
+
+            let templateParams = {
+                from_name: document.getElementById('from_name').value,
+                surname: document.getElementById('surname').value,
+                tel_number: document.getElementById('tel_number').value,
+                from_email: document.getElementById('from_email').value,
+                message: document.getElementById('message').value,
+            };
+
+            console.log(btn);
+
+            emailjs.send('default_service', 'contactForm', templateParams)
+                .then(function (response) {
+                    console.log('SUCCESS!', response.status, response.text);
+                    alert('Message Sent Successfully')
+//                    purchaseClicked()
+//                    updateCartTotal()
+                }, function (error) {
+                    console.log('FAILED...', error);
+                });
+        });
+
+    console.log(btn);
+})
+
--- assets/js/homepage-request-a-quote.js
+++ assets/js/homepage-request-a-quote.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    const btn = document.getElementById('contact-us-submit-btn');
+    const btn = document.getElementById('email-button');
     console.log(btn);
 
-    document.getElementById('contact-us-form')
+    document.getElementById('form')
         .addEventListener('submit', function (event) {
             event.preventDefault();
 
@@ -18,13 +18,16 @@ $(document).ready(function () {
                 from_name: document.getElementById('from_name').value,
                 surname: document.getElementById('surname').value,
                 tel_number: document.getElementById('tel_number').value,
-                from_email: document.getElementById('from_email').value,
-                message: document.getElementById('message').value,
+                email_address: document.getElementById('email_address').value,
+                party_date: document.getElementById('party_date').value,
+                party_location: document.getElementById('party_date').value,
+                test: document.getElementById('party_date').value,
+                options: document.getElementById('party_date').value,
             };
 
             console.log(btn);
 
-            emailjs.send('default_service', 'contactForm', templateParams)
+            emailjs.send('default_service', 'template_cewgm4l', templateParams)
                 .then(function (response) {
                     console.log('SUCCESS!', response.status, response.text);
                     alert('Message Sent Successfully')
@@ -37,3 +40,5 @@ $(document).ready(function () {
 
     console.log(btn);
 })
+
+
